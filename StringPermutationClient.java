f26dddad7cbb1db738b6247b00f8108508e46ef1
Artem-Kozlov <63732869+Artem-Kozlov@users.noreply.github.com>
2020-06-19 15:55:53 +0300

    Add files via upload

---
+++ StringPermutationClient.java
@@ -0,0 +1,11 @@
+public class StringPermutationClient {
+
+
+        public static void main(String[] args) {
+            StringPermutation printer = new StringPermutation();
+            printer.print("абвг");
+            System.out.println("Results count = " + printer.getPrintsAmount());
+        }
+
+
+}
